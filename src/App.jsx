bd68b3d6cabeeb00dd5df53dea874c60ed46b9a1
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-09-29 03:01:53 +0500

    add only jsx

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const App = () => {
+    return(
+        <>
+            <div className = "main_div">
+                <div className = "center_div">
+                    <br />
+                    <h1>ToDo List</h1>
+                    <br />
+                    <input type = "text" placeholder = "Add a Item" />
+                    <button> + </button>
+                    <ol>
+                        <li>buy Apple</li>
+                    </ol>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default App;
