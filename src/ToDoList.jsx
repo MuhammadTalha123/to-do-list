562b6ff97841bcdf1b7373a9e8d3f6642023481d
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-09-29 06:11:22 +0500

    adding cross icon for deleting todo

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const ToDoList = (props) => {
-    return <li>{props.Text}</li>;
+    return (
+    <>
+    <div className = 'todo_style'>
+     <i class = "fa fa-times" aria-hidden = "true" />
+     <li>{props.Text}</li>
+     </div>
+    </>
+    );
 };
 
 export default ToDoList;
